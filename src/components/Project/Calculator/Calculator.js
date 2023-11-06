ea8120fe505a5b944aeb09fc92191adddf27e620
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-11-06 13:16:16 +0545

    Calculator project completed

--- src/components/Project/Calculator/Calculator.js
+++ src/components/Project/Calculator/Calculator.js
@@ -12,17 +12,128 @@ export const ACTIONS = {
 };
 
 function reducer(state, { type, payload }) {
-  switch(type){
+  switch (type) {
     case ACTIONS.ADD:
-       return{
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
+      }
+      if (
+        payload.digit === "." &&
+        state.currentOperand === "." &&
+        state.currentOperand.includes(".")
+      ) {
+        return state;
+      }
+      return {
         ...state,
-        currentOperand:`${state.currentOperand || ""}${payload.digit}`
-       }
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+
+    case ACTIONS.CHOOSE_OERATION:
+      if (state.previousOperand == null && state.currentOperand == null) {
+        return state;
+      }
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          previousOperand: state.currentOperand,
+          operation: payload.operation,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+      return {
+        ...state,
+        currentOperand: evaluate(state),
+        previousOperand: null,
+        operation: null,
+        overwrite: true,
+      };
+
+    case ACTIONS.DELETE:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: null,
+          overwrite: false,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
 
     default:
-       console.log("Hello calculator");
+      console.log("Hello calculator");
   }
 }
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const cur = parseFloat(currentOperand);
+  const pre = parseFloat(previousOperand);
+  if (isNaN(cur) || isNaN(pre)) return "";
+  let result = "";
+  switch (operation) {
+    case "+":
+      result = cur + pre;
+      break;
+    case "-":
+      result = cur - pre;
+      break;
+    case "*":
+      result = cur * pre;
+      break;
+    case "%":
+      result = cur / pre;
+      break;
+  }
+  return result.toString();
+}
+
+const INTEGER_FORMAT = Intl.NumberFormat('en-us',{
+  maximumFractionDigits:0
+})
+
+function operanFormatter(operand){
+  if(operand == null) return
+  const [interger,decimal]=operand.split(".");
+  if(decimal == null) return INTEGER_FORMAT.format(interger)
+  return `${INTEGER_FORMAT.format(interger)}.${decimal}`
+} 
 
 const Calculator = () => {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -33,29 +144,39 @@ const Calculator = () => {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-          {previousOperand}
+          {operanFormatter(previousOperand)}
           {operation}
         </div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="current-operand">{operanFormatter(currentOperand)}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>﹪</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE })}>DEL</button>
+      <Operation operation="%" dispatch={dispatch} />
       <ButtonDigit digit="1" dispatch={dispatch} />
       <ButtonDigit digit="2" dispatch={dispatch} />
       <ButtonDigit digit="3" dispatch={dispatch} />
-      <button>*</button>
+      <Operation operation="*" dispatch={dispatch} />
       <ButtonDigit digit="4" dispatch={dispatch} />
       <ButtonDigit digit="5" dispatch={dispatch} />
       <ButtonDigit digit="6" dispatch={dispatch} />
-      <button>+</button>
+      <Operation operation="+" dispatch={dispatch} />
       <ButtonDigit digit="7" dispatch={dispatch} />
       <ButtonDigit digit="8" dispatch={dispatch} />
       <ButtonDigit digit="9" dispatch={dispatch} />
-      <button>-</button>
+      <Operation operation="-" dispatch={dispatch} />
       <ButtonDigit digit="0" dispatch={dispatch} />
       <ButtonDigit digit="." dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   );
 };
--- src/components/Project/Calculator/Operation.js
+++ src/components/Project/Calculator/Operation.js
@@ -3,7 +3,7 @@ import { ACTIONS } from "./Calculator";
 export default function Operation({operation, dispatch}) {
   return (
     <button
-      onClick={() => dispatch({ type: ACTIONS.ADD, payload: { operation } })}
+      onClick={() => dispatch({ type: ACTIONS.CHOOSE_OERATION, payload: { operation } })}
     >
       {operation}
     </button>
