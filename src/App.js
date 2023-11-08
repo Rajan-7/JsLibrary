77d96bebf113da9ec600c37ddc6327c9efaa8481
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-11-08 12:16:04 +0545

    Todo project initialized

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 // import UseState from "./components/Hooks/UseState";
 // import UseEffect from "./components/Hooks/UseEffect";
 // import UseReducer from "./components/Hooks/UseReducer";
-import Calculator from "./components/Project/Calculator/Calculator";
+// import Calculator from "./components/Project/Calculator/Calculator";
+import Todo from "./components/Project/t0-do/Todo";
 
 function App() {
-  return <Calculator />;
+  return <Todo />;
 }
 
 export default App;
---
+++ src/components/Project/t0-do/Todo.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+
+const Todo = () => {
+  return (
+    <>
+      <div className="todo-container">
+        <div className="todo-subcontainer">
+          <figure>
+            <img src="./images/Todo.svg" alt="todo svg format picture" />
+          </figure>
+          <figcaption className="fig-cap">Add Your List Here ✌</figcaption>
+
+          {/* ADDING ITEMS INTO To-d0 */}
+
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="🖍️ Add items..."
+              className="item-add"
+            />
+            <div className="plus">
+              <i className="fas fa-plus "></i>
+            </div>
+          </div>
+
+          {/* SHOW ITEMS OF T0-do */}
+
+          <div className="showItems">
+            <div className="eachItems">
+              <h3>Apple</h3>
+              <div className="todo-actions">
+                <i className="fas fa-edit "></i>
+                <i className="fas fa-trash "></i>
+              </div>
+            </div>
+          </div>
+
+          {/*REMOVE ALL ITEMS */}
+
+          <div className="removeItems">
+            <button className="btn effect04" data-sm-link-text="REMOVE ALL">
+              <span>CLICK</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
